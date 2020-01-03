78bc42b2869132a495fb3287e6e473b14ad5b319
Smokerdeluxe <50173125+Smokerdeluxe@users.noreply.github.com>
2020-01-03 08:28:34 +0100

    Update würfel.js

--- commands/würfel.js
+++ commands/würfel.js
@@ -1,12 +1,11 @@
-exports.run = async (client, message, args) => {
+exports.run = async (client, message, args, Discord, config, fehler, logging, fertig, warnung, privat) => {
 
-    var min = 1;
-    var max = 6
-    var Zahl = Math.floor(Math.random() * (max - min + 1)) + min;
+  var min = 1;
+  var max = 6
+  var Zahl = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    message.channel.send(
-        "Du hast eine **" + Zahl + "** gewürfelt!"
-    )
-
+  message.channel.send(
+    "Du hast eine **" + Zahl + "** gewürfelt!"
+  )
+  console.log(message.guild.id)
 }
-
